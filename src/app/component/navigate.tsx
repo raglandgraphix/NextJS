444513bfa4d81b3132dfd2123feba6e9f72343aa
John M. Ragland <john@raglandgraphix.com>
2024-10-08 16:04:44 -0500

    update

--- src/app/component/navigate.tsx
+++ src/app/component/navigate.tsx
@@ -5,9 +5,12 @@ export default function navigate(){
 
     return(
         
-        <div className="row border">
-            <div className="col-5 border">
-                <Image src="/assets/logoBlack.png" width={500} height={100} alt='Endicott logos'/>
+        <div className="row ">
+            <div className="col-2 ">
+                <Image className="img-fluid" src="/assets/logoBlack.png" width={200} height={100} alt='Endicott logos'/>
+            </div>
+            <div className="col-10 border border-danger">
+
             </div>
             
         </div>
