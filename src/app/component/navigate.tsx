2263af60d0b88a9be87a9ac04b1093602ae4e88f
John M. Ragland <john@raglandgraphix.com>
2024-10-08 16:37:05 -0500

    work on navigation

--- src/app/component/navigate.tsx
+++ src/app/component/navigate.tsx
@@ -7,16 +7,16 @@ export default function navigate(){
     return(
         
         <div className="row pt-2">
-            <div className="col-2 ">
-                <div className="row">
-                    <div className="col">
+            <div className="col-2  ">
+                <div className="row ">
+                    <div className="col d-flex justify-content-center">
                         <Image  src="/assets/logoBlack.png" width={200} height={75} alt='Endicott logos'/>
                     </div>
 
                 </div>
-                <div className="row">
-                    <div className="col">
-                        <span className="fs-6">Endicott Clay Products Company</span>
+                <div className="row text-center">
+                    <div className="col ">
+                        <span className="fs-6 ">Endicott Clay Products Company</span>
                     </div>
 
                 </div>
