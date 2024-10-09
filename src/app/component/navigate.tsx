f782369a6d75a5825f90d619205e8fa1b0e08278
John M. Ragland <john@raglandgraphix.com>
2024-10-09 14:59:17 -0500

    added the links properly and have them giving a look

--- src/app/component/navigate.tsx
+++ src/app/component/navigate.tsx
@@ -8,7 +8,7 @@ export default function navigate(){
         
         <div className="row pt-2">
             <div className="col-2  ">
-                <div className="row ">
+                <div className="row">
                     <div className="col d-flex justify-content-center">
                         <Image  src="/assets/logoBlack.png" width={200} height={75} alt='Endicott logos'/>
                     </div>
@@ -22,8 +22,8 @@ export default function navigate(){
                 </div>
                 
             </div>
-            <div className="col-10 border border-danger">
-                <span>Projects</span><span>Face Brick</span><span>Thin Brick</span><span>Pavers</span><span>Pathway Aggregate</span><span>Resource</span><span>Who We Are</span><span>Careers</span>
+            <div className="col-10 d-flex align-items-end justify-content-center  ">                
+                <span className=" p-2 pb-0 me-2 ">Projects</span><span className=" p-2 pb-0 me-2 ">Face Brick</span><span className=" p-2 pb-0 me-2 ">Thin Brick</span><span className=" p-2 pb-0 me-2 ">Pavers</span><span className=" p-2 pb-0 me-2 ">Pathway Aggregate</span><span className=" p-2 pb-0 me-2 ">Resources</span><span className=" p-2 pb-0 me-2 ">Who We Are</span><span className=" p-2 pb-0 me-2 ">Careers</span>
             </div>
             
         </div>
