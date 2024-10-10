0c818abed7ee2d622a366db2b7923c9e01f661dd
John M. Ragland <john@raglandgraphix.com>
2024-10-10 08:11:09 -0500

    updated link object
    Connected the object so it would populate the main selection by getting values from the object.

--- src/app/component/navigate.tsx
+++ src/app/component/navigate.tsx
@@ -2,13 +2,22 @@ import Image from "next/image";
 
 export default function navigate(){
 
-    const linkData = [
-        'Face Brick','Thin Brick','Pavers','Pathway Aggregate','Resources', 'Who We Are', 'Careers' 
-    ];
+    const linkData = {
+        'Projects':['All Projects','Face Brick','Thin Brick','Pavers'],
+        'Face Brick':['Colors','Textures','Sizes'],
+        'Thin Brick':['Colors','Textures','Sizes'],
+        'Pavers':['Colors','Sizes'],
+        'Pathway Aggregate':[],
+        'Resources':['Literature','BIM','Specifications','Technical Notes','Brick Briefs'],
+        'Who We Are':['Profile','Videos'],
+        'Careers':['Current Openings','Application','Solicitud','Apply Online'] 
+    };
 
 
     return(
-        
+        <div className="row">
+
+        <div className="col-12">
         <div className="row pt-2 border d-flex justify-content-center justify-content-lg-start">
             <div className="col-7 col-md-5  col-xl-3 ">
                 <div className="row ">
@@ -25,18 +34,27 @@ export default function navigate(){
                 </div>
                 
             </div>
-            <div className="col-10 d-flex align-items-end justify-content-center  "> 
+            <div className="col-19 d-flex align-items-end justify-content-end  "> 
                 {
-                    linkData.map((link)=>(
-                        <span key={link} className=" p-2 pb-0 me-2"> {link}</span>
-                    ))
+                    Object.keys(linkData).map((key)=>(
+                        <span key={key} className=" p-2 pb-0 me-2"> {key}</span>          
+                    )
+                    )
+                    
                 }               
                 
             </div>
             
             
         </div>
-        
+        <div className="row bg-black text-white p-0 m-0 d-flex justify-content-end">
+            <div className="col m-0  ">
+                <p className="text-end m-0 pt-2 pb-1">Colors</p>
+            </div>
+
+        </div>
+        </div>
+        </div>
     )
 
 }
