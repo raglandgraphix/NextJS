fdcb14b16dcdd3a078767fad65cf7efdd2ac640d
John M. Ragland <john@raglandgraphix.com>
2024-10-16 14:37:23 -0500

    made page work for all devices

--- src/app/FaceBrick/Colors/page.tsx
+++ src/app/FaceBrick/Colors/page.tsx
@@ -90,8 +90,8 @@ export default function Facebrick(){
             />
         </Head>
         <Navigate pageSettings="light"/>
-        <div className="row ">
-          <div className="col-6 faceBrickBox m-3 text-white ps-3 pt-2 pb-2 ">
+        <div className="row d-flex justify-content-between ">
+          <div className="col-12 col-xl-6 faceBrickBox ms-0 m-xl-3 text-white ps-3 pt-2 pb-2 ">
             <div className="row">
               <div className="col-6">
               <h1 className="univers-55-Oblique d-inline-block ">Face Brick</h1>
@@ -106,27 +106,27 @@ export default function Facebrick(){
             <h2 className="museo-light ">Endicott face brick: Durable and aesthetically versatile for timeless architectural design.</h2>
             <span className="univers-55-Roman">Endicott face brick elevates architecture with its rich palette of colors, from classic red and black to contemporary gray and tan.  Complementing the diverse color selection are unique textures like velour, smooth, and artisan, offering endless design possibilities for stunning and enduring facades.</span>
           </div>
-          <div className="col-12 col-xl-5 pb-2 pt-2 m-3   ">
+          <div className="col-12 col-xl-5 pb-2 pt-2 ms-xl-3  ">
             
 
-            <div className="row univers-55-Roman h-100  w-100  d-flex justify-content-xl-end">
-              <div className="col-3 border-end  text-center   ">
+            <div className="row univers-55-Roman text-rock h-100  w-100  d-flex justify-content-xl-end justify-content-center ">
+              <div className="col-4 col-md-5 col-xl-3 border-end  text-center   ">
                 <div className="row border-bottom ">
                   <div className="col ">
-                    <h2 className="fs-5">Product</h2>
+                    <h2 className="fs-5 ">Product</h2>
                   </div>
                 </div>
                 <div className="row ">
                   <div className="col text-nowrap ">
-                    <span className="d-block text-uppercase  mt-2  pt-2 pb-2 ">ALL</span>
+                    <span className="d-block text-uppercase  mt-2  pt-2 pb-2 text-rock ">ALL</span>
                     <span className="d-block text-uppercase   pt-2 pb-2 bg bg-black text-stone rounded-2">Face Brick</span>
-                    <span className="d-block text-uppercase   pt-2 pb-2">Thin Brick</span>
-                    <span className="d-block text-uppercase   pt-2 pb-2">Pavers</span>
+                    <span className="d-block text-uppercase   pt-2 pb- text-rock">Thin Brick</span>
+                    <span className="d-block text-uppercase   pt-2 pb-2 text-rock">Pavers</span>
                   </div>
                 </div>
                 
               </div>
-              <div className="col-5 text-center">
+              <div className="col-7 col-md-5 col-xl-6 text-center">
                 <div className="row">
                   <div className="col border-bottom">
                     <h2 className="fs-5 ">Color Range</h2>
@@ -134,11 +134,11 @@ export default function Facebrick(){
                 </div>
                 <div className="row">
                   <div className="col">
-                    <Link className="text-decoration-none text-black" href="/FaceBrick/Colors-ALL"><span className={`d-block text-uppercase  mt-2 pt-2 pb-2  ${selectedColor===null || selectedColor==='ALL'?'bg bg-black text-stone rounded-2':''} `}>All</span></Link>
-                    <Link className="text-decoration-none text-black" href="/FaceBrick/Colors-Red"> <span className={`d-block text-uppercase   pt-2 pb-2  ${selectedColor==='Red' || selectedColor==='Burgundy'?'bg bg-black text-stone rounded-2':''} `}>Red/Burgundy</span></Link>
-                    <Link className="text-decoration-none text-black" href='/FaceBrick/Colors-Black'><span className={`d-block text-uppercase   pt-2 pb-2 ${selectedColor==='Black' || selectedColor==='Plum'?'bg bg-black text-stone rounded-2':''}`} >Black/Plum</span></Link>
-                    <Link className="text-decoration-none text-black" href='/FaceBrick/Colors-Gray'><span className={`d-block text-uppercase   pt-2 pb-2 ${selectedColor==='Gray' || selectedColor==='White'|| selectedColor==='Cream' || selectedColor==='Buff'?'bg bg-black text-stone rounded-2':''}`}>Gray/White/Cream/Buff</span></Link>
-                    <Link className="text-decoration-none text-black" href='/FaceBrick/Colors-Tan'><span className={`d-block text-uppercase   pt-2 pb-2 ${selectedColor==='Tan' || selectedColor==='Brown' || selectedColor==='Orange'?'bg bg-black text-stone rounded-2':''}`}>Tan/Brown/Orange</span></Link>
+                    <Link className="text-decoration-none text-rock" href="/FaceBrick/Colors-ALL"><span className={`d-block text-uppercase  mt-2 pt-2 pb-2  ${selectedColor===null || selectedColor==='ALL'?'bg bg-black text-stone rounded-2':''} `}>All</span></Link>
+                    <Link className="text-decoration-none text-rock" href="/FaceBrick/Colors-Red"> <span className={`d-block text-uppercase   pt-2 pb-2  ${selectedColor==='Red' || selectedColor==='Burgundy'?'bg bg-black text-stone rounded-2':''} `}>Red/Burgundy</span></Link>
+                    <Link className="text-decoration-none text-rock " href='/FaceBrick/Colors-Black'><span className={`d-block text-uppercase   pt-2 pb-2 ${selectedColor==='Black' || selectedColor==='Plum'?'bg bg-black text-stone rounded-2':''}`} >Black/Plum</span></Link>
+                    <Link className="text-decoration-none text-rock" href='/FaceBrick/Colors-Gray'><span className={`d-block text-uppercase   pt-2 pb-2 ${selectedColor==='Gray' || selectedColor==='White'|| selectedColor==='Cream' || selectedColor==='Buff'?'bg bg-black text-stone rounded-2':''}`}>Gray/White/Cream/Buff</span></Link>
+                    <Link className="text-decoration-none text-rock" href='/FaceBrick/Colors-Tan'><span className={`d-block text-uppercase   pt-2 pb-2 ${selectedColor==='Tan' || selectedColor==='Brown' || selectedColor==='Orange'?'bg bg-black text-stone rounded-2':''}`}>Tan/Brown/Orange</span></Link>
                   </div>
                 </div>
                 
@@ -146,13 +146,13 @@ export default function Facebrick(){
             </div>
           </div>
         </div>
-        <div className="row">
+        <div className="row d-flex justify-content-center justify-content-md-start">
           {
             data.map((item)=>(
               selectedColor === 'ALL' || selectedColor === null || item.colorGroup.some(color => (selectedColor ?? '').includes(color)) ? (
                 
-              <div key={item.id} className="col-4">
-                <div className="Card border rounded-bottom rounded-3 m-1  ">
+              <div key={item.id} className=" col-11 col-md-6 col-xl-4 mb-5">
+                <div className="Card border rounded-bottom-4 m-1 shadow text-rock">
                   <Image className="card-img-top img-fluid" width={500} height={500} alt={item.altTag} src={item.mainImage}  />
                     <div className="card-body">
                       <div className="card-title text-center">
