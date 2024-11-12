13e9cff12f5c774c7a520e1fa3ac32bc8309eb9b
John M. Ragland <john@raglandgraphix.com>
2024-11-12 08:38:27 -0600

    removed commented code

--- src/app/component/ColorProjects.tsx
+++ src/app/component/ColorProjects.tsx
@@ -39,10 +39,7 @@ export default function ColorProjects(){
           const result = await FetchProduct(Product);
           setData(result);
 
-          //if (result) { // Only fetch ProjectData if Data is available
-           // const projectResult = await FetchProjects(Product);
-            //setProjectData(projectResult);
-          //}
+        
         };
     
         getData();
@@ -79,46 +76,7 @@ export default function ColorProjects(){
             ))
         }
            
-           {/* <div className="row">
-           <div className="col-2 ">
-                <div className="card border ">
-                 <Image className="d-inline-block me-2 w-100" src="https://endicottfiles.com/EastPrairieBox.jpg" width={200} height={200} alt="Image of East Prairie School"/>
-                <div className="card-body">
-                    <h5 className="card-title fs-6">
-                        East Prairie Elementary School
-                    </h5>
-
-                </div>
-                </div>
-                </div>
-                <div className="col-2 ">
-                <div className="card border ">
-                <Image className="d-inline-block me-2 w-100" src="https://endicottfiles.com/FortMeyersSquare.jpg" width={200} height={200} alt="Image of East Prairie School"/>
-                <div className="card-body">
-                    <h5 className="card-title fs-6 d-inline me-1">
-                        Fort Meyer Public Library
-                    </h5>
-                    <Image className="d-inline" src="/assets/MiscImages/Images.png" alt="Image that lets people know that the card is a link to images of Fort Meyer" width={25} height={25}/>
-
-                </div>
-                </div>
-                </div>
-                <div className="col-2 ">
-                <div className="card border ">
-                <Image className="d-inline-block me-2 w-100" src="https://endicottfiles.com/GothamBox.jpg" width={200} height={200} alt="Image of East Prairie School"/>
-                <div className="card-body">
-                    <h5 className="card-title fs-6">
-                        Gotham West Apartments
-                    </h5>
-
-                </div>
-                </div>
-                </div>
-                
-                
-                
-            
-           </div> */}
+         
            <div className="row">
             {
                 Data.map((item)=>(
