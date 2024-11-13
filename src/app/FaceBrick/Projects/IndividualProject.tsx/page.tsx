9387be0fbe2b83389e34e7a48b5b453095efe901
John M. Ragland <john@raglandgraphix.com>
2024-11-13 10:10:56 -0600

    trying something new
    // eslint-disable-next-line no-unused-vars

--- src/app/FaceBrick/Projects/IndividualProject.tsx/page.tsx
+++ src/app/FaceBrick/Projects/IndividualProject.tsx/page.tsx
@@ -3,10 +3,26 @@ import React from "react";
 import { useState,useEffect } from "react";
 import { FetchProjects } from "../../../../../Utilities/FetchProjects";
 import { Job } from "../../../../../Types/ProjectTypes";
+import { usePathname } from "next/navigation";
 
 export default function IndividualProject(){
     const [Product,setProduct]=useState<string | null>(null);
+    // eslint-disable-next-line no-unused-vars
     const [ProjectData,setProjectData]=useState<Job[] | null>(null);
+    // eslint-disable-next-line no-unused-vars
+    const [RangeColors,setRangeColors]=useState<string | null>(null);
+    const pathname=usePathname();
+    useEffect(()=>{
+        const parts = pathname.split('/');
+        if(parts.length>1){
+            setProduct(parts[1]);
+            setRangeColors( parts[2].includes('-')?pathname.split('-')[1]:null);
+        }
+
+
+    })
+    
+   
 useEffect(() => {
     const getProjectData = async () => {
       const result = await FetchProjects(Product);
