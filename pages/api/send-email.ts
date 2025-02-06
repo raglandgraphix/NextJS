ac0340a807d12f4d4544f314ff47d7db74843c08
John M. Ragland <john@raglandgraphix.com>
2025-02-06 12:33:45 -0600

    Update send-email.ts

--- pages/api/send-email.ts
+++ pages/api/send-email.ts
@@ -1,7 +1,7 @@
 // pages/api/send-email.ts
 'use client'
 import { NextApiRequest, NextApiResponse } from 'next';
-//import sendgrid from '@sendgrid/mail';
+import sendgrid from '@sendgrid/mail';
 
 // Initialize SendGrid with your API key.  **IMPORTANT:** Store this securely, ideally as an environment variable.
 //const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
@@ -17,7 +17,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // }
     try {
       //const { position,firstName, middleName,lastName,StreetAddress } = req.body; // Extract the 'name' from the form data
-      
+      const { firstName } = req.body; // Extract the 'name' from the form data
+      console.log(firstName)
       // Construct the email message
       // const msg = {
       //   to: 'jragland@endicott.com', // Replace with your recipient email
