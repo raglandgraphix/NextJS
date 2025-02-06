0c4040442e4b6e2e7a4c1132eb9b277788359400
John M. Ragland <john@raglandgraphix.com>
2025-02-06 13:42:39 -0600

    Update send-email.ts

--- pages/api/send-email.ts
+++ pages/api/send-email.ts
@@ -13,7 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   //const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
   //const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
   if (req.method === 'POST') {
-    console.log(SENDGRID_API_KEY);
+    
     if (process.env.SENDGRID_API_KEY) {
       sendgrid.setApiKey(process.env.SENDGRID_API_KEY); 
     }
